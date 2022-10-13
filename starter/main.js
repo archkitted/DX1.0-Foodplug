6b137e071aa6287e25fbb194b133feeab4c10e97
SoetanED <elizabethsoetan031@gmail.com>
2022-10-13 13:43:54 +0100

    Started Harburger Section

---
+++ starter/main.js
@@ -0,0 +1,15 @@
+
+
+// console.log("send me your account number")
+
+// var writeup = "I play alot"
+
+
+// console.log(document)
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+console.log(hamburgerdiv)
+function dosomething() {
+    hamburgerdiv.classList.toggle("showburger")
+}
